c681d987af1dcf4eefa552511496e5741ade4eb8
Webm07 <webm@webmmedia.org>
2025-04-24 16:12:43 -0400

    c

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,12 +3,14 @@ import ws.schild.jave.MultimediaObject;
 
 public class Main {
 	
-	public void init() {
+	public static void init() {
+		System.out.println("Ayo");
 		// Run initalization of program, start UI, etc.
 		// This will run when the app is started.
 	}
 
 	public static void main(String[] args) {
 		System.out.println("Attempting to initialize EchoMaster...");
+		init();
 	}
 }
