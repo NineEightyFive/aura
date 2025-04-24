b865eae6b5c49e148286e9c38fa5ae23ddc5e3ba
DanBennettJeffreys <DanBennettJeffreys@landmark.edu>
2025-04-24 16:34:11 -0400

    Initial Useless UI
    Shows a blank window that does nothing but quits the program when you close it.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,5 @@
+import UI.EchoFrame;
+import javax.swing.JFrame;
 import ws.schild.jave.Encoder;
 import ws.schild.jave.MultimediaObject;
 
@@ -6,9 +8,17 @@ public class Main {
 	public void init() {
 		// Run initalization of program, start UI, etc.
 		// This will run when the app is started.
+                System.out.println("Attempting to initialize EchoMaster...");
+                EchoFrame ef = new EchoFrame();
+                ef.setSize(512, 512);
+                ef.setTitle("Echo Master");
+                ef.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                ef.setVisible(true);
 	}
 
 	public static void main(String[] args) {
-		System.out.println("Attempting to initialize EchoMaster...");
+                Main main = new Main();
+                main.init();
+		
 	}
 }
