7b40ca615b128d81bd3f91b15ea8808384646c7e
DanBennettJeffreys <DanBennettJeffreys@landmark.edu>
2025-05-01 17:13:02 -0400

    Possibly working dynamic metadata list
    use public methods at start of UI/MetadataPanel.java to change presentation of metadata.

--- src/main/java/UI/EchoFrame.java
+++ src/main/java/UI/EchoFrame.java
@@ -6,6 +6,11 @@ package UI;
 
 import javax.swing.JFrame;
 import java.awt.GridBagLayout;
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Arrays;
+import java.util.HashSet;
 import javax.swing.JPanel;
 import javax.swing.JButton;
 import javax.swing.JTextField;
@@ -18,19 +23,24 @@ import javax.swing.JComboBox;
  */
 @SuppressWarnings("unused")
 public class EchoFrame extends JFrame{
+    public MetadataPanel mdPanel;
+    
     public EchoFrame(){
         String[] metadataKeys = {
-            "author",
+            "artist",
             "title",
             "date"
         };
-        String[] defaultOptions = {
-            "<keep>",
-            "<clear>"
-        };
         
-        this.setLayout(new GridBagLayout());
-        MetadataPanel panel = new MetadataPanel((String[])metadataKeys, (String[])defaultOptions);
-        this.add(panel);
+        HashSet<String> mandatoryKeys = new HashSet<>();
+        mandatoryKeys.addAll(Arrays.asList(metadataKeys));
+        
+        
+        this.setLayout(new BorderLayout());
+        PrimaryButtonPanel buttonPanel = new PrimaryButtonPanel();
+        mdPanel = new MetadataPanel(mandatoryKeys);
+        this.add(buttonPanel, BorderLayout.PAGE_START);
+        this.add(mdPanel, BorderLayout.CENTER);
+        
     }
 }
--- src/main/java/UI/MetadataPanel.java
+++ src/main/java/UI/MetadataPanel.java
@@ -9,26 +9,90 @@ import javax.swing.BoxLayout;
 import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import javax.swing.JTextField;
+import javax.swing.JLabel;
 import javax.swing.JComboBox;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 /**
  *
  * @author Dan Bennett-Jeffreys
  */
 @SuppressWarnings("unused")
-public class MetadataPanel extends JPanel{
+public final class MetadataPanel extends JPanel{
     /*private String name;
     private JTextField label;
     private JComboBox metadata;*/
-    
+    private final HashSet<String> mandatoryKeyset;
+    private HashSet<String> keys;
+    private HashMap<String,MDSingleKeyPanel> keyPanels;
+    private HashMap<String,ArrayList<String>> inputValues;
+    private HashMap<String,String> outputValues;
 
+    public HashSet<String> getKeyset(){
+        return keys;
+    }
+    
+    public void setKeyset(HashSet<String> newKeys){
+        newKeys.addAll(mandatoryKeyset);
+        keys.addAll(newKeys);
+        for(String singleKey: keys){
+            MDSingleKeyPanel panel = keyPanels.get(singleKey);
+            if(newKeys.contains(singleKey)){
+                if(panel == null){
+                    panel = new MDSingleKeyPanel(singleKey);
+                    this.add(panel);
+                    keyPanels.put(singleKey, panel);
+                    //inputValues.put(singleKey, new ArrayList<>());
+                    outputValues.put(singleKey, panel.getOutputValue());
+                }
+            }else{
+                if(panel != null){
+                    this.remove(panel);
+                    keyPanels.remove(singleKey);
+                    inputValues.remove(singleKey);
+                    outputValues.remove(singleKey);
+                }
+            }
+        }
+        this.revalidate();
+        this.repaint();
+        keys = newKeys;
+    }
+    
+    public HashMap<String,ArrayList<String>> getInputValues(){
+        return inputValues;
+    }
+    
+    public void setInputValues(HashMap<String,ArrayList<String>> newInputValues){
+        inputValues = newInputValues;
+        for (Map.Entry<String,MDSingleKeyPanel> entry : keyPanels.entrySet()){
+            entry.getValue().setInputValues(inputValues.get(entry.getKey()));
+        }
+    }
+    
+    public HashMap<String,String> getOutputValues(){
+        return outputValues;
+    }
     
     private void metadataChangedByUI(String key, String value){
-        //TODO: integrate with rest of EchoMaster
+        outputValues.put(key, value);
         System.out.println("metadata changed: key=\""+key+"\" value=\"" + value+"\"");
     }
     
-    public MetadataPanel(String[] keys, String[] defaults){
+    public MetadataPanel(HashSet<String> keyset){
+        this.mandatoryKeyset = keyset;
+        keys = new HashSet<>();
+        keyPanels = new HashMap<>();
+        inputValues = new HashMap<>();
+        outputValues = new HashMap<>();
+        setKeyset(new HashSet());
+    }
+    
+    /*public MetadataPanel(String[] keys, String[] defaults){
         this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
         for (String key: keys){
             JPanel subPanel = new JPanel();
@@ -44,12 +108,44 @@ public class MetadataPanel extends JPanel{
             //TODO: make a (key,valueBox) map so code can change displayed metadata
             // ** Dan see MetaLink.changemeta(key,val) -o
         }
-    }
+    }*/
     
         public static void doesThisExist(){
         System.out.println("The MetadataPanel class exists");
     }
     
+    private class MDSingleKeyPanel extends JPanel{
+        private static final String[] defaultChoices = {"<keep>", "<clear>"};
+        private final JLabel label;
+        private final JComboBox valueBox;
+        public MDSingleKeyPanel(String key){
+            this.setLayout(new FlowLayout());
+            label = new JLabel(key);
+            valueBox = new JComboBox(defaultChoices);
+            valueBox.setEditable(true);
+            valueBox.addActionListener(new MetadataComboBoxListener(key));
+            this.add(label);
+            this.add(valueBox);
+        }
+        
+        public void setInputValues(ArrayList<String> inputValues){
+            valueBox.removeAllItems();
+            for(String value: defaultChoices){
+                valueBox.addItem(value);
+            }
+            if(inputValues != null){
+                for(String value: inputValues){
+                    valueBox.addItem(value);
+                }
+            }
+        }
+        
+        public String getOutputValue(){
+            return (String)valueBox.getSelectedItem();
+        }
+    }
+        
+        
     public class MetadataComboBoxListener implements ActionListener {
         private final String key;
         
