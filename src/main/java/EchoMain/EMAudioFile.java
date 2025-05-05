3a41e0e44c217f1b626265e5acedcbf0b442c34c
Webm07 <webm@webmmedia.org>
2025-05-05 13:48:03 -0400

    Java Docs

--- src/main/java/EchoMain/EMAudioFile.java
+++ src/main/java/EchoMain/EMAudioFile.java
@@ -29,8 +29,8 @@ public class EMAudioFile {
 
 	
 	/**
-	 * @param key
-	 * @return
+	 * @param key The file metadata object to pull (samplingrate, decoder, bitrate, channels)
+	 * @return The value of the provided key, if any
 	 */
 	public String getSingleAudioMetadata(String key) {
 		if (key.equals("samplingrate")) return String.valueOf(info.getAudio().getSamplingRate());
@@ -40,6 +40,11 @@ public class EMAudioFile {
 		return null; // Would return data
 	};
 	
+	/**
+	 * 
+	 * @param key The file metadata object to pull (samplingrate, decoder, bitrate, channels)
+	 * @param value The new value to change the data to
+	 */
 	public void setSingleAudioMetadata(String key, String value) {
 		if (key.equals("samplingrate")) info.getAudio().setSamplingRate(0);
 		else if (key.equals("decoder")) info.getAudio().setDecoder(value);
@@ -52,16 +57,27 @@ public class EMAudioFile {
 		return metaData;
 	}
 
+	/**
+	 * Returns the path of the file
+	 * @author Owain L.
+	 * @return A string with the full file path
+	 */
 	public String getPath() {
 		return file.getPath();
 	}
 
 	
-
+/**
+ * 
+ * @return The new file format to convert the file to, default is MP3
+ */
 	public String getNewFormat() {
 		return newFormat;
 	}
-
+	/**
+	 * 
+	 * @param format The new file format to convert the file to
+	 */
 	public void setNewFormat(String format) {
 		newFormat = format;
 	}
@@ -74,10 +90,19 @@ public class EMAudioFile {
 		return file;
 	}
 
+	/**
+	 * Returns the name of the file
+	 * @author Owain L.
+	 * @return The name of the file, including its appropriate format
+	 */
 	public String getFileName() {
 		return file.getName();
 	}
-
+	/**
+	 * General constructor for a new AudioFile, added to the masterlist in the UI class
+	 * @param path The string path of the file
+	 * @throws EncoderException
+	 */
 	public EMAudioFile(String path) throws EncoderException {
 		file = new File(path);
 		mmObject = new MultimediaObject(file);
